d98bab83b645be8a44b3bc8ff37fe2a87612b3c7
babluroy <bablu.roy@batchservice.com>
2022-10-30 02:20:12 +0530

    caraousel modified

--- Components/Carousel/index.js
+++ Components/Carousel/index.js
@@ -1,11 +1,16 @@
 import React, { Fragment } from 'react'
+import Link from 'next/link';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import { url_constants } from '../../utils/routerLink_constants';
 
 export default function CarouselComp( { caraouselBlogs } ) {
   return (
+    <Fragment>
     <Carousel autoFocus={true} dynamicHeight={true} showThumbs={false}>
       {caraouselBlogs.map((data, index) => (
+      <>
+       <Link href={url_constants.blog + data._id}>
       <div key={index}>
         <img src={data.image} alt={data.title}  style={
         {
@@ -16,7 +21,10 @@ export default function CarouselComp( { caraouselBlogs } ) {
       }/>
         <p className="legend">{data.title}</p>
       </div>
+      </Link>
+      </>
       ))}
     </Carousel>
+    </Fragment>
   );
 }
