0e039b2f568769df3b696d4b2efc49c2488578de
babluroy <bablu.roy@batchservice.com>
2022-10-31 22:59:58 +0530

    loader added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import Head from 'next/head';
+import Router from "next/router";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import Layout from '../Components/Layout'
 import Footer from '../Components/Footer'
@@ -6,11 +8,32 @@ import '../styles/globals.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { UserContext } from '../Context/UserContext';
-import Head from 'next/head';
+import Loader from "../Components/Loader"
+
 
 function MyApp({ Component, pageProps }) {
 
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("finished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
 
   return (
     <>
@@ -34,7 +57,11 @@ function MyApp({ Component, pageProps }) {
         draggable
         pauseOnHover
       />
-      <Component {...pageProps} />
+      {loading ? (
+        <Loader loader={loading}/>
+      ) : 
+        <Component {...pageProps} />
+      }
       {/* <Footer/> */}
       </UserContext.Provider>
     </>
