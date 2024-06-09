5a8c477f6182bf2bed9ed36b832428f8dbe8c3d5
babluroy <bablu.roy@batchservice.com>
2024-06-10 01:52:47 +0530

    build error fixed

--- pages/app/home.js
+++ pages/app/home.js
@@ -9,9 +9,7 @@ import Link from 'next/link';
 import { url_constants } from "../../utils/routerLink_constants"
 import CategoryCard from '../../Components/CategoryCard';
 
-export default function Home( { caraouselBlogs, featuredBlogs, allBlogs, categories } )
-{
-
+export default function Home({ caraouselBlogs, featuredBlogs, allBlogs, categories }) {
   return (
     <>
       <CarouselComp caraouselBlogs={caraouselBlogs} />
@@ -19,29 +17,29 @@ export default function Home( { caraouselBlogs, featuredBlogs, allBlogs, categor
       <MDBContainer className="mt-5">
         <h3 className="text-center mb-4">Featured Blogs</h3>
         <MDBRow>
-          {featuredBlogs.map( ( data, index ) => (
+          {(featuredBlogs || []).map((data, index) => (
             <MDBCol md='4' lg='4' sm='12' key={index}>
               <BlogCard title={data.title} shortDesc={data.shortDesc} image={data.image} id={data._id} />
             </MDBCol>
-          ) )}
+          ))}
         </MDBRow>
 
         <MDBRow className="mt-5">
           <h3 className="text-center mb-4">Read By Categories</h3>
-          {categories.map( ( data, index ) => (
+          {(categories || []).map((data, index) => (
             <MDBCol md='3' lg='3' sm='6' xs="6" key={index}>
               <CategoryCard category={data.name} id={data._id} />
             </MDBCol>
-          ) )}
+          ))}
         </MDBRow>
 
         <MDBRow className="mt-5">
           <h3 className="text-center mb-4">All Blogs</h3>
-          {allBlogs.map( ( data, index ) => (
+          {(allBlogs || []).map((data, index) => (
             <MDBCol md='4' lg='4' sm='12' key={index}>
               <BlogCard title={data.title} shortDesc={data.shortDesc} image={data.image} id={data._id} />
             </MDBCol>
-          ) )}
+          ))}
         </MDBRow>
         <div className="d-grid gap-2 col-6 mx-auto mt-4 pb-5">
           <Link href={url_constants.allBlogs}>
@@ -53,19 +51,18 @@ export default function Home( { caraouselBlogs, featuredBlogs, allBlogs, categor
   )
 }
 
-export const getStaticProps = async () =>
-{
+export const getStaticProps = async () => {
   const caraouselBlogs = await getHighlightedBlogs();
   const featuredBlogs = await getFeaturedBlogs();
-  const allBlogs = await getAllBlogs( 50 );
+  const allBlogs = await getAllBlogs(50);
   const categories = await getCategories();
+
   return {
     props: {
-      caraouselBlogs: caraouselBlogs || null,
-      featuredBlogs: featuredBlogs || null,
-      allBlogs: allBlogs || null,
-      categories: categories || null,
+      caraouselBlogs: caraouselBlogs || [],
+      featuredBlogs: featuredBlogs || [],
+      allBlogs: allBlogs || [],
+      categories: categories || [],
     }
   }
 }
-
